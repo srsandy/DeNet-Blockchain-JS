cbad749524fd62cbaa325d8483200bdb7521a54d
srsandy <sandeepranjan2007@yahoo.co.in>
2018-06-06 16:25:33 +0530

    consensus route added

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -48,7 +48,7 @@ app.post('/transaction/brodcast', (req, res) => {
     });
 });
 
-app.get('/mine', function(req, res) {
+app.get('/mine', (req, res) => {
     const lastBlock = bitcoin.getLastBlock();
     const previousBlockHash = lastBlock['hash'];
 
@@ -176,6 +176,48 @@ app.post('/register-nodes-bulk', (req, res) => {
 
     res.json({
         note: 'Bulk registration successfully'
+    });
+});
+
+app.get('/consensus', (req, res) => {
+    const requestPromises = [];
+    bitcoin.networkNodes.forEach(networkNodeUrl => {
+    
+        const requestOptions = {
+            uri: networkNodeUrl + '/blockchain',
+            method: 'GET',
+            json: true
+        };
+
+        requestPromises.push(rp(requestOptions));
+    });
+
+    Promise.all(requestPromises).then(blockchains => {
+        const currentChainLength = bitcoin.chain.length;
+        let maxChainLength = currentChainLength;
+        let newLongestChain = null;
+        let newPendingTransactions = null;
+        blockchains.forEach(blockchain => {
+            if(blockchain.chain.length > maxChainLength) {
+                maxChainLength = blockchain.chain.length;
+                newLongestChain = blockchain.chain;
+                newPendingTransactions = blockchain.pendingTransactions;
+            }  
+        }); 
+
+        if(!newLongestChain || (newLongestChain && !bitcoin.chainIsValid(newLongestChain))) {
+            res.json({
+                note: 'Current chain has not been replaced',
+                chain: bitcoin.chain
+            }); 
+        } else if(newLongestChain && bitcoin.chainIsValid(newLongestChain)) {
+            bitcoin.chain = newLongestChain;
+            bitcoin.pendingTransactions = newPendingTransactions;
+            res.json({
+                note: 'this chain has been replaced',
+                chain: bitcoin.chain
+            });
+        }
     });
 });
 
